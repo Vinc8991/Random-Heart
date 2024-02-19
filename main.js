2ca0dbcad2237a992f57a7f06b656ae3c6992c9e
Vinc8991 <160488983+Vinc8991@users.noreply.github.com>
2024-02-19 16:58:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+const heartCreate = () => {
+    const heart = document.createElement('div');
+    heart.classList.add('heartrain');
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.animationDuration = Math.random() * 2 + 3 + "s";   
+    heart.innerText  =  "💖";
+    document.body.appendChild(heart);
+
+    setTimeout(()=>{
+        heart.remove();
+    },5000);
+    
+}
+setInterval(heartCreate,  300);
